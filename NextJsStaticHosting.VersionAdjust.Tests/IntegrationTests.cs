cd4dce693b3bcb46308b92fcb88c94b3fee853f8
bymse <egorstrelov@gmail.com>
2023-10-22 21:16:51 +0300

    _next/static integration tests

--- NextJsStaticHosting.VersionAdjust.Tests/IntegrationTests.cs
+++ NextJsStaticHosting.VersionAdjust.Tests/IntegrationTests.cs
@@ -46,6 +46,21 @@ public class IntegrationTests : IDisposable
         Assert.Equal(expectedContent, response);
     }
 
+    [Theory]
+    [InlineData("/_next/static/chunks/main-current.js", "main-current")]
+    [InlineData("/_next/static/chunks/pages/index-current.js", "index-current")]
+    [InlineData("/_next/static/chunks/pages/id/[id]-current.js", "[id]-current")]
+    [InlineData("/_next/static/css/first-current.css", "first-current")]
+    [InlineData("/_next/static/css/second-current.css", "second-current")]
+    [InlineData("/_next/static/hash-current/_buildManifest.js", "_buildManifest")]
+    [InlineData("/_next/static/hash-current/_ssgManifest.js", "_ssgManifest")]
+    [InlineData("/_next/static/media/font-current.woff2", "font-current")]
+    public async Task ShouldReturnCurrentVersion_OfStaticFiles(string path, string expectedContent)
+    {
+        var response = await httpClient.GetStringAsync(path);
+        Assert.Equal(expectedContent, response);
+    }
+
     public void Dispose()
     {
         testServer.Dispose();
