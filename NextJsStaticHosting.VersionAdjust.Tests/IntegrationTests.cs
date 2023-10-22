4dec7d5f51d3fd2f6732cbe88ed38e4c295a7ed3
bymse <egorstrelov@gmail.com>
2023-10-22 21:04:04 +0300

    Tests for static and dynamic routes

--- NextJsStaticHosting.VersionAdjust.Tests/IntegrationTests.cs
+++ NextJsStaticHosting.VersionAdjust.Tests/IntegrationTests.cs
@@ -21,7 +21,26 @@ public class IntegrationTests : IDisposable
     [Theory]
     [InlineData("/", "index")]
     [InlineData("/index.html", "index")]
-    public async Task ShouldReturnCurrentVersionOfPages(string path, string expectedContent)
+    [InlineData("/about.html", "about")]
+    [InlineData("/about", "about")]
+    [InlineData("/inner/page.html", "inner-page")]
+    [InlineData("/inner/page", "inner-page")]
+    public async Task ShouldReturnCurrentVersion_OfStaticRoutes(string path, string expectedContent)
+    {
+        var response = await httpClient.GetStringAsync(path);
+        Assert.Equal(expectedContent, response);
+    }
+    
+    [Theory]
+    [InlineData("/1/2", "[first][second]")]
+    [InlineData("/1093/2398", "[first][second]")]
+    [InlineData("/id/2398", "[id]")]
+    [InlineData("/id/test", "[id]")]
+    [InlineData("/catchall/other", "catch all")]
+    [InlineData("/catchall/first/second", "catch all")]
+    [InlineData("/catchalloptional/other", "catch all optional")]
+    [InlineData("/catchalloptional/first/second", "catch all optional")]
+    public async Task ShouldReturnCurrentVersion_OfDynamicRoutes(string path, string expectedContent)
     {
         var response = await httpClient.GetStringAsync(path);
         Assert.Equal(expectedContent, response);
