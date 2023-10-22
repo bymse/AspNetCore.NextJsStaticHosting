adea49a9d85390d6d6a90f73d607b597d2e8e547
bymse <egorstrelov@gmail.com>
2023-10-22 22:36:29 +0300

    Fixes in routes provider test

--- NextJsStaticHosting.VersionAdjust.Tests/NextJsRoutesProviderTests.cs
+++ NextJsStaticHosting.VersionAdjust.Tests/NextJsRoutesProviderTests.cs
@@ -11,21 +11,25 @@ public class NextJsRoutesProviderTests
     {
         var expected = new[]
             {
-                new FileRoute("about.html", "about.html"),
-                new FileRoute("about", "about.html"),
-                new FileRoute("index.html", "index.html"),
                 new FileRoute("", "index.html"),
-                new FileRoute("idslug/{id}.html", "idslug/[id].html"),
-                new FileRoute("idslug/{id}", "idslug/[id].html"),
-                new FileRoute("inner/page.html", "inner/page.html"),
+                new FileRoute("index.html", "index.html"),
+                new FileRoute("about", "about.html"),
+                new FileRoute("about.html", "about.html"),
+                new FileRoute("404", "404.html"),
+                new FileRoute("404.html", "404.html"),
+                new FileRoute("id/{id}", "id/[id].html"),
+                new FileRoute("id/{id}.html", "id/[id].html"),
+                new FileRoute("two-slugs/{first}/{second}", "two-slugs/[first]/[second].html"),
+                new FileRoute("two-slugs/{first}/{second}.html", "two-slugs/[first]/[second].html"),
                 new FileRoute("inner/page", "inner/page.html"),
+                new FileRoute("inner/page.html", "inner/page.html"),
                 new FileRoute("catchall/{**slug}", "catchall/[...slug].html"),
                 new FileRoute("catchalloptional/{**slug}", "catchalloptional/[[...slug]].html"),
-                
             }.OrderBy(e => e.Route)
             .ToArray();
+
         var fileProvider = new PhysicalFileProvider(
-            Path.Combine(Environment.CurrentDirectory, "TestFiles")
+            TestFilesPathProvider.CurrentVersion
         );
 
         var actual = NextJsPageFileRoutesProvider
