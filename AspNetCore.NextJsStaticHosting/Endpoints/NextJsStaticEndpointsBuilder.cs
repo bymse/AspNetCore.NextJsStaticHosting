62e1aa611995997f5e4ccabbe8b4b36200bc6df2
bymse <egorstrelov@gmail.com>
2023-10-24 20:54:47 +0300

    Fix exception in LoadEndpoints on reload

--- AspNetCore.NextJsStaticHosting/Endpoints/NextJsStaticEndpointsBuilder.cs
+++ AspNetCore.NextJsStaticHosting/Endpoints/NextJsStaticEndpointsBuilder.cs
@@ -10,11 +10,9 @@ internal static class NextJsStaticEndpointsBuilder
 {
     public static IEnumerable<Endpoint> Build(
         FileRoute[] routes,
-        IEndpointRouteBuilder endpoints,
-        NextJsStaticEndpointsOptions options
+        RequestDelegate requestDelegate
     )
     {
-        var requestDelegate = GetRequestDelegate(endpoints, options);
         foreach (var route in routes)
         {
             var builder = new RouteEndpointBuilder(
@@ -28,43 +26,5 @@ internal static class NextJsStaticEndpointsBuilder
 
             yield return builder.Build();
         }
-    }
-
-    private static RequestDelegate GetRequestDelegate(IEndpointRouteBuilder endpoints, NextJsStaticEndpointsOptions options)
-    {
-        var app = endpoints.CreateApplicationBuilder();
-        app.Use(next => context =>
-        {
-            var endpoint = context.GetEndpoint();
-            var metadata = endpoint?.Metadata.GetMetadata<StaticEndpointMetadata>();
-            if (metadata == null)
-            {
-                throw new InvalidOperationException(
-                    $"{nameof(StaticEndpointMetadata)} not found on endpoint: {endpoint}"
-                );
-            }
-
-            context.Request.Path = "/" + metadata.Path;
-            context.SetEndpoint(null);
-
-            return next(context);
-        });
-
-        app.UseStaticFiles(new StaticFileOptions
-        {
-            FileProvider = options.FileProvider
-        });
-
-        return app.Build();
-    }
-
-    private class StaticEndpointMetadata
-    {
-        public StaticEndpointMetadata(string path)
-        {
-            Path = path;
-        }
-
-        public string Path { get; }
     }
 }
